46bc5ea25b94c5771dc615ca84debfc89100b683
bminczu <bminczu@gmail.com>
2021-04-21 15:43:06 -0500

    routing

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,35 @@
-console.log("JS is loaded")
+console.log("JS is loaded"); 
+
+const router = async () => {
+    const routes = [
+        {path: "/", view: () => console.log("viewing Dashboard")},
+        {path: "/posts", view: () => console.log("viewing posts")},
+        {path: "/settings", view: () => console.log("viewing settings")},
+        
+    ];
+
+
+    // test each route for potential matches
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if (!match) {
+        match = {
+            route: routes[0], 
+            isMatch: true
+        };
+    
+    }
+    console.log(match)
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    router();
+});
